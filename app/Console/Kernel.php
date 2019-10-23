0b26d60e30c673f61e1644a2eca5e8b1f8b7db34
AustinMaddox <austin@maddoxbox.com>
2019-10-22 19:55:57 -0600

    Add cron service

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,6 +24,14 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        /*
+         * Note: The only thing we should be putting on a schedule here is QUEUED JOBS or QUEUED CONSOLE COMMANDS.
+         * This schedule is triggered with the `php artisan schedule:run` command from `MotoMenus-SaaS-API-Cron`.
+         * The stack defined in `MotoMenus-SaaS-API-Cron` is not suitable for running anything significant, has the bare-minimum as far as PHP extensions, and is not designed to do any heavy lifting.
+         * The `MotoMenus-SaaS-API-Queue` service is well-equipped with enough memory and all the required PHP extensions and such to run any scheduled processes/tasks/jobs we need.
+         * Since `MotoMenus-SaaS-API-Cron` is the one that ultimately calls this schedule, anything put here should be a queued job that will run in the `MotoMenus-SaaS-API-Queue` service.
+         */
+
         // $schedule->command('inspire')
         //          ->hourly();
     }
